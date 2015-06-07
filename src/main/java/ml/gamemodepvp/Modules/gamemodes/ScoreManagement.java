e65139c5435b3be0ec3e3502a063f56b04a0ab82
bfox1 <robert.gamiz@yahoo.com>
2015-06-07 10:08:08 -0700

    Updating before leave

--- src/main/java/ml/gamemodepvp/Modules/gamemodes/ScoreManagement.java
+++ src/main/java/ml/gamemodepvp/Modules/gamemodes/ScoreManagement.java
@@ -9,6 +9,5 @@ public class ScoreManagement {
     /**
      * ScoreManagement will handle the ScoreBoard of the Arena it is assigned to. Having access to ALL players located
      * within the arena and being able to send correct ScoreBoard to the players
-     * 
      */
 }
---
+++ src/main/java/ml/gamemodepvp/Modules/gamemodes/freeforall/FreeForAll.java
@@ -0,0 +1,10 @@
+package ml.gamemodepvp.Modules.gamemodes.freeforall;
+
+import ml.gamemodepvp.Modules.gamemodes.Gamemode;
+
+/**
+ * Created by bfox1 on 6/7/2015.
+ * In God We Trust.
+ */
+public class FreeForAll extends Gamemode {
+}
